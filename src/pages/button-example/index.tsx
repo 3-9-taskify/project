c7c04067dd8c395869cba5f5e4b7ec29f640ae96
김우현 <rladngus133@naver.com>
2024-01-25 16:38:16 +0900

    chore: resBtn add styles(flex, Mobaile img width)

--- src/pages/button-example/index.tsx
+++ src/pages/button-example/index.tsx
@@ -1,13 +1,17 @@
 import ResponseBtn from "@/components/commons/button/ResponseButton";
 import { SignButton } from "@/components/commons/button/SignButton";
 import { MixButton } from "@/components/commons/button/MixButton";
+import Image from "next/image";
 
 export default function Index() {
   return (
     <>
       <div style={{ display: "flex", gap: "1rem", padding: "1rem" }}>
         {/*state  : accept | reject | cancel | delete*/}
-        <ResponseBtn state="accept">수락</ResponseBtn>
+        <ResponseBtn state="accept">
+          <Image src="/assets/images/plus.png" height={16} width={16} alt="sd" />
+          수락
+        </ResponseBtn>
         <ResponseBtn state="reject">취소</ResponseBtn>
         <ResponseBtn state="cancel">거절</ResponseBtn>
         <ResponseBtn state="delete">삭제</ResponseBtn>
