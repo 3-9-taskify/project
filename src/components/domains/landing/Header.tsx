2d53bdd297e7ff0ad29ba34065d2211d8d096655
김우현 <rladngus133@naver.com>
2024-01-26 19:25:57 +0900

    feat: add Header to the landing page

---
+++ src/components/domains/landing/Header.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import HeaderImg from "./Header-assets/section.png";
+import ResponseBtn from "@/components/commons/button/ResponseButton";
+import styles from "./Header.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+export default function Header() {
+  return (
+    <section className={cx("container")}>
+      <div className={cx("img-container")}>
+        <Image src={HeaderImg} fill alt="SectionImg" />
+      </div>
+      <div className={cx("bottom-content-container")}>
+        <div className={cx("text-container")}>
+          <div className={cx("text-h2-container")}>
+            <h2>새로운 일정 관리&nbsp;</h2>
+            <h2>Taskify</h2>
+            {/* Montserrat 폰트사용 개인파일사용 / 전역사용 ? */}
+          </div>
+          <p>나만의 자유로운 일정관리 Taskify</p>
+        </div>
+        <ResponseBtn state="accept" ph={1.5} pw={14}>
+          로그인하기
+        </ResponseBtn>
+      </div>
+    </section>
+  );
+}
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -2,6 +2,8 @@ import classNames from "classnames/bind";
 import React from "react";
 import styles from "@/pages/landing/index.module.scss";
 import Navbar from "@/components/domains/landing/Navbar";
+import Container from "@/components/domains/landing/Container";
+import Header from "@/components/domains/landing/Header";
 
 const cx = classNames.bind(styles);
 
@@ -9,6 +11,9 @@ export default function index() {
   return (
     <div className={cx("container")}>
       <Navbar />
+      <Container>
+        <Header />
+      </Container>
     </div>
   );
 }
