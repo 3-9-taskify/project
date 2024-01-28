71ae0aec4d9bee048bf4a1d3554a589680ff8710
bjiwon324 <148836373+bjiwon324@users.noreply.github.com>
2024-01-28 23:17:24 +0900

    feat: Edit Page main

---
+++ src/components/commons/colorList/index.tsx
@@ -0,0 +1,30 @@
+import { useState, MouseEvent } from "react";
+import styles from "./colorList.module.scss";
+import classNames from "classnames/bind";
+import Image from "next/image";
+
+const cx = classNames.bind(styles);
+
+const colors = ["green", "purple", "orange", "blue", "pink"];
+
+export default function ColorList() {
+  const [selectedColor, setSelectedColor] = useState<string>("");
+
+  function handleSelectColor(color: string) {
+    if (selectedColor === color) {
+      setSelectedColor("");
+    } else {
+      setSelectedColor(color);
+    }
+  }
+
+  return (
+    <ul className={cx("color-list")}>
+      {colors.map((color) => (
+        <li onClick={() => handleSelectColor(color)} id={color} className={cx("color-btn", color)}>
+          {selectedColor === color && <Image src="/assets/icons/ic-check.svg.svg" fill alt="선택" />}
+        </li>
+      ))}
+    </ul>
+  );
+}
