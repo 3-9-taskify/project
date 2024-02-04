dfedb7aba2a8daf0140a2e37f819c81accd06da7
김우현 <rladngus133@naver.com>
2024-02-05 00:06:08 +0900

    feat: add infinity scroll Logic

--- src/api/getReceivedDashboardInvitations.ts
+++ src/api/getReceivedDashboardInvitations.ts
@@ -1,13 +1,14 @@
 import { axiosInstance } from "./axiosInstance";
 
-export default async function getReceivedDashboardInvitations(accessToken: string | null) {
+export default async function getReceivedDashboardInvitations(cursorId: number | null, accessToken: string | null) {
   try {
     const res = await axiosInstance.get(`invitations`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
       params: {
-        size: 10,
+        size: 5,
+        cursorId: cursorId,
       },
     });
 
